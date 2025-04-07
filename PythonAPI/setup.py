a829fac63b723f3a6e0ac1052a434d130bea6116
mfourmy <mederic.fourmy@gmail.com>
2025-04-07 22:54:33 +0200

    Switch to PEP 518 -> simplify build dependencies, no need to install numpy before pycocotools installation

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,26 +1,17 @@
-from setuptools import setup, Extension
+# setup.py (minimal shim for dynamic configuration)
+from setuptools import setup
 import numpy as np
 
-# To compile and install locally run "python setup.py build_ext --inplace"
-# To install library to Python site-packages run "python setup.py build_ext install"
+def build_with_numpy():
+    from setuptools import Extension
+    ext_modules = [
+        Extension(
+            'pycocotools._mask',
+            sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
+            include_dirs=[np.get_include(), '../common'],
+            extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        )
+    ]
+    return {"ext_modules": ext_modules}
 
-ext_modules = [
-    Extension(
-        'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
-    )
-]
-
-setup(
-    name='pycocotools',
-    packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
-    install_requires=[
-        'setuptools>=18.0',
-        'cython>=0.27.3'
-    ],
-    version='2.0',
-    ext_modules= ext_modules
-)
+setup(**build_with_numpy())
